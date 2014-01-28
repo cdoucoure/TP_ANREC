20d097d3981664bbff23bcb3d42eb75a5756541c
cdoucoure <clara.doucoure@gmail.com>
2014-01-28 15:13:41 +0100

    Create Donnees.java

---
+++ Donnees.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+
+public class Donnees {
+
+	/**
+	 * @param args
+	 */
+	public LinkedList<Point> points;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
