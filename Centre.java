e4cf8afede7203f62b5d2ed1b4860e86bf481f21
cdoucoure <clara.doucoure@gmail.com>
2014-01-28 15:13:15 +0100

    Create Centre.java

---
+++ Centre.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class Centre extends Point{
+
+	/**
+	 * @param args
+	 */
+	
+	public Centre(){
+		super();
+	}
+	
+	public void deplacer(LinkedList<Point> points){
+		
+	}
+
+}
