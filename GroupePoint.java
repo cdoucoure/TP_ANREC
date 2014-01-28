26daf24b2937f9a41c4c9872cc17defdf28005f5
cdoucoure <clara.doucoure@gmail.com>
2014-01-28 15:14:10 +0100

    Create GroupePoint.java

---
+++ GroupePoint.java
@@ -0,0 +1,39 @@
+import java.awt.Color;
+import java.util.LinkedList;
+
+
+
+public class GroupePoint {
+	
+	public Centre centre;
+	public Color couleur;
+	public String nom;	
+	
+	
+	// Methode permettant de calculer le centre d'un groupe de points
+	public void calculerCentre(Donnees donne){
+		int longueur = donne.points.getFirst().coord.size();
+		Double[] coordonnesCentre= new Double[longueur];// Liste des points ayant pour groupe this
+		int i=0;
+		LinkedList<Point> pointsCentre=null;
+		
+		for (Point p : donne.points){
+			if (p.groupe==this){
+				pointsCentre.add(p);
+			}
+		}
+		
+		// calcul des coordonnes du nouveau point
+		for (Point p : pointsCentre){
+			i=0;
+			for(i=0; i<p.coord.size(); i++){
+				coordonnesCentre[i]=coordonnesCentre[i] + p.coord.get(i);
+			}
+		}
+		
+		i=0;
+		for(i=0; i<longueur; i++){
+			this.centre.coord.set(i, coordonnesCentre[i]/longueur);
+		}
+	}
+}
